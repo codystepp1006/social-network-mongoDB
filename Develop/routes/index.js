4bc10c75e3c0d95a3d4ae3cb08af73f21caef0f4
Cody Stepp <codystepp691@gmail.com>
2024-03-12 22:31:37 -0500

    fixed user and thought routes by updating get put post delete methods, also cleaned up commented out code

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -23,15 +23,15 @@ router.route('/api/createUser').post(createUser);
 router.route('/api/getSingleUser').get(getSingleUser);
 router.route('/api/deleteUser').delete(deleteUser);
 router.route('/api/updateUser').put(updateUser);
-router.route('/api/addFriend').get(addFriend);
-router.route('/api/removeFriend').get(removeFriend);
+router.route('/api/addFriend').post(addFriend);
+router.route('/api/removeFriend').delete(removeFriend);
 
 //thought routes
 router.route('/api/getThoughts').get(getThoughts);
 router.route('/api/createThought').post(createThought);
 router.route('/api/getSingleThought').get(getSingleThought);
-router.route('/api/removeReaction').get(removeReaction);
-router.route('/api/addReaction').get(addReaction);
-router.route('/api/deleteThought').get(deleteThought);
+router.route('/api/removeReaction').delete(removeReaction);
+router.route('/api/addReaction').put(addReaction);
+router.route('/api/deleteThought').delete(deleteThought);
 
 module.exports = router;
